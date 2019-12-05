df7b4e2ba0378caed4af64ca9d4a7e66586b32dc
Sam Smith <smith.10439@osu.edu>
2019-12-05 11:43:14 -0500

    Added arrays

--- Bubble Sort/MyPlayground.playground/Contents.swift
+++ Bubble Sort/MyPlayground.playground/Contents.swift
@@ -6,3 +6,25 @@ print("before:",array)
 print("after:", bubbleSort(array))
 print("after:", bubbleSort(array, <))
 print("after:", bubbleSort(array, >))
+
+struct DFArray<T> {
+    var data: [T]
+    var count: Int
+    private var capacity: Int
+    
+    init(_ size: Int) {
+        data = []
+        count = 0
+        capacity = size
+    }
+    
+    subscript(index: Int) -> T {
+        get {
+            return data[index]
+        }
+        
+        set(newValue) {
+            data[index] = newValue
+        }
+    }
+}
